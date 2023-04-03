85e96dcbb972071bcf705e42aea66211ee66981e
Gonzalo <gonzaloezemolina@gmail.com>
2023-04-03 19:25:26 -0300

    Carrito interfaz

--- index.js
+++ index.js
@@ -97,11 +97,12 @@ const productos = [
 //Dom
 const contenedorProductos = document.querySelector("#contenedor-productos")
 let productoBotones = document.querySelectorAll(".producto__btn")
+const carritoContainer = document.querySelector("#carrito-contenedor");
 const contadorCarrito = document.querySelector("#contador__carrito")
 
 
 
-// div con la class producto y las siguientes caracteristicas de la tarjeta de producto.
+// Inyectar las tarjetas al html. div con la class producto y las siguientes caracteristicas de la tarjeta de producto.
 function cargarProductos() {
 
     contenedorProductos.innerHTML="";
@@ -135,7 +136,11 @@ function btnProduct() {
     })
 }
 
-// Funcion agregar al carrito
+
+// Carrito array
+const carrito = [];
+
+// Funcion agregar al carrito array + cantidad + contador
 function añadirAlCarrito(e) {
     const idBtn = e.currentTarget.id;
 const productoAdd = productos.find(producto => producto.id === idBtn);
@@ -145,22 +150,127 @@ if(carrito.some(producto => producto.id === idBtn)){
 } else {
     productoAdd.cantidad = 1;
 carrito.push(productoAdd);
+cargarCarrito();
+console.log(carrito);
 }
+
 
 //Funcion para actualizar el contador del carrito
 function cantidadCarrito () {
     let contador = carrito.reduce((acc, producto) => acc + producto.cantidad, 0)
     contadorCarrito.innerText = contador;
 }
-
 cantidadCarrito();
+
 
 //Local Storage
 localStorage.setItem("productos-en-carrito" , JSON.stringify(carrito))
 }
 
 
+// Funcion para cargar productos en el carrito
+const cargarCarrito = () => { 
+    carritoContainer.innerHTML = "" 
 
-// Carrito array
-const carrito = [];
+    carrito.forEach((producto) => {
+        const divCarrito = document.createElement('div')
+        divCarrito.classList.add ('productoEnCarrito')
+        divCarrito.innerHTML = `
+        <p>${producto.titulo}</p>
+        <p>Precio:$${producto.precio}</p>
+        <p>Cantidad: <span id="cantidad">${producto.cantidad}</span></p>
+        <button (${producto.id}) class="boton-eliminar"><i class="bi bi-trash-fill"></i></button>
+        `
 
+        carritoContainer.appendChild(divCarrito)
+        
+        localStorage.setItem('carrito', JSON.stringify(carrito))
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// agregarProductoCarrito();
+
+// function agregarProductoCarrito(producto) { // Función para agregar un producto al carrito en la interfaz
+//     const carritoItem = document.createElement('div');
+//     carritoItem.classList.add('productoEnCarrito');
+//     carritoItem.dataset.id = producto.id;
+//     carritoItem.innerHTML = `
+//     <p>${producto.nombre}</p>
+//         <p>Precio:$${producto.precio}</p>
+//         <p>Cantidad: <span id="cantidad">${producto.cantidad}</span></p>
+//     <button class="boton-eliminar"><i class="bi bi-trash-fill"></i></button>
+//     `;
+//   carritoContainer.appendChild(carritoItem);
+//   // Agregar event listeners para los botones de aumentar y disminuir cantidad y eliminar producto
+// //   carritoItem.querySelector('.aumentar-cantidad').addEventListener('click', () => aumentarCantidad(producto.id));
+// //   carritoItem.querySelector('.disminuir-cantidad').addEventListener('click', () => disminuirCantidad(producto.id));
+//   carritoItem.querySelector('.boton-eliminar').addEventListener('click', () => {eliminarProducto(producto.id, carritoItem);}); 
+// }
+
+
+// function aumentarCantidad(id) { // Función para aumentar la cantidad de un producto en el carrito
+    
+//     const itemCarrito = carrito.find((item) => item.id === id); // Busca el ítem del carrito con el ID proporcionado  
+//     if (itemCarrito) { // Si se encuentra el ítem, incrementa su cantidad
+//           itemCarrito.cantidad += 1;
+//           if (itemCarrito.cantidad < 1) {
+//             eliminarProducto(id);
+//           } 
+//           actualizarCarritoEnInterfaz();
+//           actualizarTotalCarrito();
+//     }
+// }    
+
+// function disminuirCantidad(id) { // Función para disminuir la cantidad de un producto en el carrito
+        
+//         const itemCarrito = carrito.find((item) => item.id === id); // Busca el ítem del carrito con el ID proporcionado
+//         if (itemCarrito && itemCarrito.cantidad > 1) { // Si se encuentra el ítem y su cantidad es mayor a 1, decrementa su cantidad
+//             itemCarrito.cantidad--;   
+//             actualizarTotalCarrito();
+//             actualizarCarritoEnInterfaz();// Actualiza el total del carrito
+//         }
+// }
+    
+// function eliminarProducto(id, carritoItem) { // Función para eliminar un producto del carrito
+//     if (!carritoItem) { // Si el elemento HTML no se encuentra como parámetro, busca el elemento correspondiente en el DOM
+//         carritoItem = carritoContainer.querySelector(`[data-id="${id}"]`);
+//     }
+//     const index = carrito.findIndex((item) => item.id === id); // Encuentra el índice del ítem del carrito con el ID proporcionado
+//     if (index !== -1) { // Si se encuentra el ítem, elimínalo del array del carrito y de la interfaz gráfica
+//         carrito.splice(index, 1);
+//         carritoContainer.removeChild(carritoItem);
+//         mostrarTotalYPedido();
+//         actualizarTotalCarrito(); // Actualiza el total del carrito
+//         if (carrito.length === 0) { // Si el carrito está vacío, oculta los elementos de "total" y "hacer pedido"
+//           carritoTotalElement.classList.add("oculto");
+//           if (hacerPedidoElement) {
+//             hacerPedidoElement.classList.add("oculto-boton");
+//           }
+//         }
+//         localStorage.setItem("carrito", JSON.stringify(carrito));
+//       }
+//       else {
+//         console.error("El objeto carrito[index] es undefined");
+//      
